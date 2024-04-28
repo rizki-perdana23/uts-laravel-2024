2a139402b89fad6a7e01d7408905342422dbaa98
rizki-perdana23 <rperdana184@gmail.com>
2024-04-28 11:04:16 +0700

    Belajar FOR

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,12 @@ Route::get('profile', function () {
     return view('profile');
 });
 
-
 Route::get('produk', function () {
     return view('produk');
+});
+
+Route::get('array', function () {
+    for ($i = 1; $i <= 5; $i++) {
+        echo 'hello world' . $i  . 'x<br';
+    }
 });
